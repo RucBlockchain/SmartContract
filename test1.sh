507cb8dcfffb94bd80923d2e7cc9e8c0fb327af1
RucBlockchain <33385772+RucBlockchain@users.noreply.github.com>
2018-05-30 16:56:38 +0800

    Update test1.sh

--- test1.sh
+++ test1.sh
@@ -49,7 +49,7 @@ echo "################### check_buyTicket ######################"
 echo "##########################################################"
 echo
 sleep 2
-boolValue=`peer chaincode query -n mycc -v 1.0 -c '{"Args":["getRandBool"]}' -C myc`
+boolValue=`peer chaincode query -n mycc -v 1.0 -c '{"Args":["check_buyTicket"]}' -C myc`
 echo $boolValue
 
 if [ "$boolValue" = "Query Result: false" ] ; then
@@ -88,19 +88,6 @@ else
 
   sleep 3
 
-  #echo
-  #echo "##########################################################"
-  #echo "######################## timeOut #########################"
-  #echo "##########################################################"
-  #echo
-  #sleep 2
-  #peer chaincode invoke -n mycc  -v 1.0 -c '{"Args":["timeOut","11"]}' -C myc
-
-  #sleep 1
-  #echo "Client didn't deposit during the required time!"
-
-  #exit 1
-
   act2="{'Args':['clientDeposit','1','1','1','1','12:00']}"
   r2="2da67e2fe36b96be6f042c3e308396caaa6f83058e0a344f57a3bf725cd93aac"
   s2="58a15127bd9ce4d051e89a54be12d4ccfb80fdff16d34b8ebdc85d3da56348bf"
@@ -114,29 +101,6 @@ else
   peer chaincode invoke -n mycc  -v 1.0 -c '{"Args":["clientDeposit","1","1","1","1","12:00","'${act2}'","'${r2}'","'${s2}'"]}' -C myc
 
   sleep 3
-
-  #echo
-  #echo "##########################################################"
-  #echo "######################## timeOut #########################"
-  #echo "##########################################################"
-  #echo
-  #sleep 2
-  #peer chaincode invoke -n mycc  -v 1.0 -c '{"Args":["timeOut","11"]}' -C myc
-
-  #sleep 1
-  #echo "Insurance company didn't deposit during the required time!"
-
-  #echo
-  #echo "##########################################################"
-  #echo "######################## withdraw ########################"
-  #echo "##########################################################"
-  #echo
-  #sleep 2
-  #peer chaincode invoke -n mycc  -v 1.0 -c '{"Args":["clientWithdraw","11"]}' -C myc
-
-  #sleep 1
-
-  #exit 1
 
   echo
   echo "##########################################################"
@@ -173,7 +137,7 @@ else
   echo "##########################################################"
   echo
   sleep 2
-  boolValue=`peer chaincode query -n mycc  -v 1.0 -c '{"Args":["getRandBool"]}' -C myc`
+  boolValue=`peer chaincode query -n mycc  -v 1.0 -c '{"Args":["check_delay"]}' -C myc`
   echo $boolValue
 
   if [ "$boolValue" = "Query Result: true" ] ; then
