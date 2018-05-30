3d355d206ded1473e2d02f31ecde47b995cecc28
RucBlockchain <33385772+RucBlockchain@users.noreply.github.com>
2018-05-30 16:48:13 +0800

    Update fsmEvent.go

--- fsmEvent.go
+++ fsmEvent.go
@@ -305,48 +305,48 @@ func clientWithdraw(stub shim.ChaincodeStubInterface, args []string) (pb.Respons
 
   // ==== Create client compositekey ====
 clientIndexName := "client"
-	clientIndexKey, err := stub.CreateCompositeKey(clientIndexName, []string{clientID})
-	if err != nil {
-		return shim.Error(err.Error()), ""
-	}
-	value := []byte{0x00}
+  clientIndexKey, err := stub.CreateCompositeKey(clientIndexName, []string{clientID})
+  if err != nil {
+    return shim.Error(err.Error()), ""
+  }
+  value := []byte{0x00}
 
-	// ==== Check if client exists ====
-	clientAsBytes, err := stub.GetState(clientIndexKey)
-	if err != nil {
-		return shim.Error("Failed to get client: " + err.Error()), ""
-	} else if clientAsBytes == nil {
-		return shim.Error("Client doesn't exit, please check your client ID!"), ""
-	}
+  // ==== Check if client exists ====
+  clientAsBytes, err := stub.GetState(clientIndexKey)
+  if err != nil {
+    return shim.Error("Failed to get client: " + err.Error()), ""
+  } else if clientAsBytes == nil {
+    return shim.Error("Client doesn't exit, please check your client ID!"), ""
+  }
 
-	err = json.Unmarshal([]byte(clientAsBytes), &clientJSON)
-	if err != nil {
-		jsonResp := "{\"Error\":\"Failed to decode JSON of: " + clientID + "\"}"
-		return shim.Error(jsonResp), ""
-	}
+  err = json.Unmarshal([]byte(clientAsBytes), &clientJSON)
+  if err != nil {
+    jsonResp := "{\"Error\":\"Failed to decode JSON of: " + clientID + "\"}"
+    return shim.Error(jsonResp), ""
+  }
 
-	objectType := clientJSON.ObjectType
-	x := clientJSON.PublicKeyX
-	y := clientJSON.PublicKeyY
-	account := clientJSON.Account + 100
+  objectType := clientJSON.ObjectType
+  x := clientJSON.PublicKeyX
+  y := clientJSON.PublicKeyY
+  account := clientJSON.Account + 100
 
-	verify := verifySignature(x, y, r, s, act)
-	if verify == false {
-		return shim.Error("Verify failed!!!"), ""
-	}
+  verify := verifySignature(x, y, r, s, act)
+  if verify == false {
+    return shim.Error("Verify failed!!!"), ""
+  }
 
-	newClientJSON := &Client{clientID, objectType, x, y, account}
-	clientJSONAsBytes, err := json.Marshal(newClientJSON)
-	if err != nil {
-		return shim.Error(err.Error()), ""
-	}
-	stub.PutState(clientIndexKey, value)
+  newClientJSON := &Client{clientID, objectType, x, y, account}
+  clientJSONAsBytes, err := json.Marshal(newClientJSON)
+  if err != nil {
+    return shim.Error(err.Error()), ""
+  }
+  stub.PutState(clientIndexKey, value)
 
-	// ==== Save client to state ====
-	err = stub.PutState(clientIndexKey, clientJSONAsBytes)
-	if err != nil {
-		return shim.Error(err.Error()), ""
-	}
+  // ==== Save client to state ====
+  err = stub.PutState(clientIndexKey, clientJSONAsBytes)
+  if err != nil {
+    return shim.Error(err.Error()), ""
+  }
 
   return shim.Success(nil), "Done"
 }
@@ -356,66 +356,66 @@ clientIndexName := "client"
 //        changeOrder - save ticketStatus into state
 // ==========================================================
 func changeOrder(stub shim.ChaincodeStubInterface, args []string) (pb.Response, string) {
-	var orderJSON Order
+  var orderJSON Order
 
   //       0            1             2
-	// "GuaranteeID", "OrderID", "TicketStatus"
+  // "GuaranteeID", "OrderID", "TicketStatus"
   if len(args) != 3 {
-		return shim.Error("Incorrect number of arguments. Expecting 3"), ""
-	}
+    return shim.Error("Incorrect number of arguments. Expecting 3"), ""
+  }
 
   if len(args[0]) <= 0 {
-		return shim.Error("1st argument must be a non-empty string"), ""
-	}
-	if len(args[1]) <= 0 {
-		return shim.Error("2nd argument must be a non-empty string"), ""
-	}
-	if len(args[2]) <= 0 {
-		return shim.Error("3rd argument must be a non-empty string"), ""
-	}
+    return shim.Error("1st argument must be a non-empty string"), ""
+  }
+  if len(args[1]) <= 0 {
+    return shim.Error("2nd argument must be a non-empty string"), ""
+  }
+  if len(args[2]) <= 0 {
+    return shim.Error("3rd argument must be a non-empty string"), ""
+  }
 
   orderID := args[1]
-	ticketStatus := args[2]
+  ticketStatus := args[2]
 
-	// ==== Create order compositekey ====
-	orderIndexName := "order"
-	orderIndexKey, err := stub.CreateCompositeKey(orderIndexName, []string{orderID})
-	if err != nil {
-		return shim.Error(err.Error()), ""
-	}
-	value := []byte{0x00}
+  // ==== Create order compositekey ====
+  orderIndexName := "order"
+  orderIndexKey, err := stub.CreateCompositeKey(orderIndexName, []string{orderID})
+  if err != nil {
+    return shim.Error(err.Error()), ""
+  }
+  value := []byte{0x00}
 
-	// ==== Check if order exists ====
-	orderAsBytes, err := stub.GetState(orderIndexKey)
-	if err != nil {
-		return shim.Error("Failed to get order: " + err.Error()), ""
-	} else if orderAsBytes == nil {
-		return shim.Error("Order doesn't exit, please check your order ID!"), ""
-	}
+  // ==== Check if order exists ====
+  orderAsBytes, err := stub.GetState(orderIndexKey)
+  if err != nil {
+    return shim.Error("Failed to get order: " + err.Error()), ""
+  } else if orderAsBytes == nil {
+    return shim.Error("Order doesn't exit, please check your order ID!"), ""
+  }
 
-	err = json.Unmarshal([]byte(orderAsBytes), &orderJSON)
-	if err != nil {
-		jsonResp := "{\"Error\":\"Failed to decode JSON of: " + orderID + "\"}"
-		return shim.Error(jsonResp), ""
-	}
+  err = json.Unmarshal([]byte(orderAsBytes), &orderJSON)
+  if err != nil {
+    jsonResp := "{\"Error\":\"Failed to decode JSON of: " + orderID + "\"}"
+    return shim.Error(jsonResp), ""
+  }
 
-	clientID := orderJSON.ClientID
-	airlineID := orderJSON.AirlineID
-	objectType := orderJSON.ObjectType
-	createTime := orderJSON.CreateTime
+  clientID := orderJSON.ClientID
+  airlineID := orderJSON.AirlineID
+  objectType := orderJSON.ObjectType
+  createTime := orderJSON.CreateTime
 
-	newOrderJSON := &Order{orderID, clientID, airlineID, objectType, createTime, ticketStatus}
-	orderJSONAsBytes, err := json.Marshal(newOrderJSON)
-	if err != nil {
-		return shim.Error(err.Error()), ""
-	}
-	stub.PutState(orderIndexKey, value)
+  newOrderJSON := &Order{orderID, clientID, airlineID, objectType, createTime, ticketStatus}
+  orderJSONAsBytes, err := json.Marshal(newOrderJSON)
+  if err != nil {
+    return shim.Error(err.Error()), ""
+  }
+  stub.PutState(orderIndexKey, value)
 
-	// ==== Save guarantee to state ====
-	err = stub.PutState(orderIndexKey, orderJSONAsBytes)
-	if err != nil {
-		return shim.Error(err.Error()), ""
-	}
+  // ==== Save guarantee to state ====
+  err = stub.PutState(orderIndexKey, orderJSONAsBytes)
+  if err != nil {
+    return shim.Error(err.Error()), ""
+  }
 
   return shim.Success(nil), "Done"
 }
@@ -425,79 +425,79 @@ func changeOrder(stub shim.ChaincodeStubInterface, args []string) (pb.Response, 
 //        insurCompanyWithdraw - refund cash deposit to insurance company
 // ===============================================================================
 func insurCompanyWithdraw(stub shim.ChaincodeStubInterface, args []string) (pb.Response, string) {
-	var insuranceCompanyJSON InsuranceCompany
+  var insuranceCompanyJSON InsuranceCompany
 
   //       0                1            2       3      4
-	// "GuaranteeID", "InsurCompanyID", '$act2', "$r2", "$s2"
+  // "GuaranteeID", "InsurCompanyID", '$act2', "$r2", "$s2"
   if len(args) != 5 {
-		return shim.Error("Incorrect number of arguments. Expecting 5"), ""
-	}
+    return shim.Error("Incorrect number of arguments. Expecting 5"), ""
+  }
 
   if len(args[0]) <= 0 {
-		return shim.Error("1st argument must be a non-empty string"), ""
-	}
-	if len(args[1]) <= 0 {
-		return shim.Error("2nd argument must be a non-empty string"), ""
-	}
-	if len(args[2]) <= 0 {
-		return shim.Error("3rd argument must be a non-empty string"), ""
-	}
-	if len(args[3]) <= 0 {
-		return shim.Error("4th argument must be a non-empty string"), ""
-	}
-	if len(args[4]) <= 0 {
-		return shim.Error("5th argument must be a non-empty string"), ""
-	}
+    return shim.Error("1st argument must be a non-empty string"), ""
+  }
+  if len(args[1]) <= 0 {
+    return shim.Error("2nd argument must be a non-empty string"), ""
+  }
+  if len(args[2]) <= 0 {
+    return shim.Error("3rd argument must be a non-empty string"), ""
+  }
+  if len(args[3]) <= 0 {
+    return shim.Error("4th argument must be a non-empty string"), ""
+  }
+  if len(args[4]) <= 0 {
+    return shim.Error("5th argument must be a non-empty string"), ""
+  }
 
-	insurCompanyID := args[1]
-	act := args[2]
-	r := args[3]
-	s := args[4]
+  insurCompanyID := args[1]
+  act := args[2]
+  r := args[3]
+  s := args[4]
 
-	// ==== Create insurance company compositekey ====
-	companyIndexName := "company"
-	companyIndexKey, err := stub.CreateCompositeKey(companyIndexName, []string{insurCompanyID})
-	if err != nil {
-		return shim.Error(err.Error()), ""
-	}
-	value := []byte{0x00}
+  // ==== Create insurance company compositekey ====
+  companyIndexName := "company"
+  companyIndexKey, err := stub.CreateCompositeKey(companyIndexName, []string{insurCompanyID})
+  if err != nil {
+    return shim.Error(err.Error()), ""
+  }
+  value := []byte{0x00}
 
-	// ==== Check if insurance company exists ====
-	companyAsBytes, err := stub.GetState(companyIndexKey)
-	if err != nil {
-		return shim.Error("Failed to get insurance company: " + err.Error()), ""
-	} else if companyAsBytes == nil {
-		return shim.Error("Insurance company doesn't exit, please check your insurance company ID!"), ""
-	}
+  // ==== Check if insurance company exists ====
+  companyAsBytes, err := stub.GetState(companyIndexKey)
+  if err != nil {
+    return shim.Error("Failed to get insurance company: " + err.Error()), ""
+  } else if companyAsBytes == nil {
+    return shim.Error("Insurance company doesn't exit, please check your insurance company ID!"), ""
+  }
 
-	err = json.Unmarshal([]byte(companyAsBytes), &insuranceCompanyJSON)
-	if err != nil {
-		jsonResp := "{\"Error\":\"Failed to decode JSON of: " + insurCompanyID + "\"}"
-		return shim.Error(jsonResp), ""
-	}
+  err = json.Unmarshal([]byte(companyAsBytes), &insuranceCompanyJSON)
+  if err != nil {
+    jsonResp := "{\"Error\":\"Failed to decode JSON of: " + insurCompanyID + "\"}"
+    return shim.Error(jsonResp), ""
+  }
 
-	objectType := insuranceCompanyJSON.ObjectType
-	x := insuranceCompanyJSON.PublicKeyX
-	y := insuranceCompanyJSON.PublicKeyY
-	account := insuranceCompanyJSON.Account + 10100
+  objectType := insuranceCompanyJSON.ObjectType
+  x := insuranceCompanyJSON.PublicKeyX
+  y := insuranceCompanyJSON.PublicKeyY
+  account := insuranceCompanyJSON.Account + 10100
 
-	verify := verifySignature(x, y, r, s, act)
-	if verify == false {
-		return shim.Error("Verify failed!!!"), ""
-	}
+  verify := verifySignature(x, y, r, s, act)
+  if verify == false {
+    return shim.Error("Verify failed!!!"), ""
+  }
 
-	newCompanyJSON := &InsuranceCompany{insurCompanyID, objectType, x, y, account}
-	companyJSONAsBytes, err := json.Marshal(newCompanyJSON)
-	if err != nil {
-		return shim.Error(err.Error()), ""
-	}
-	stub.PutState(companyIndexKey, value)
+  newCompanyJSON := &InsuranceCompany{insurCompanyID, objectType, x, y, account}
+  companyJSONAsBytes, err := json.Marshal(newCompanyJSON)
+  if err != nil {
+    return shim.Error(err.Error()), ""
+  }
+  stub.PutState(companyIndexKey, value)
 
-	// ==== Save insurance company to state ====
-	err = stub.PutState(companyIndexKey, companyJSONAsBytes)
-	if err != nil {
-		return shim.Error(err.Error()), ""
-	}
+  // ==== Save insurance company to state ====
+  err = stub.PutState(companyIndexKey, companyJSONAsBytes)
+  if err != nil {
+    return shim.Error(err.Error()), ""
+  }
 
   return shim.Success(nil), "Done"
 }
@@ -507,79 +507,79 @@ func insurCompanyWithdraw(stub shim.ChaincodeStubInterface, args []string) (pb.R
 //        indemnify - refund cash deposit to client
 // ==========================================================
 func indemnify(stub shim.ChaincodeStubInterface, args []string) (pb.Response, string) {
-	var clientJSON Client
+  var clientJSON Client
 
   //       0             1         2       3      4
-	// "GuaranteeID", "ClientID", "$act4", "$r4", "$s4"
+  // "GuaranteeID", "ClientID", "$act4", "$r4", "$s4"
   if len(args) != 5 {
-		return shim.Error("Incorrect number of arguments. Expecting 5"), ""
-	}
+    return shim.Error("Incorrect number of arguments. Expecting 5"), ""
+  }
 
   if len(args[0]) <= 0 {
-		return shim.Error("1st argument must be a non-empty string"), ""
-	}
-	if len(args[1]) <= 0 {
-		return shim.Error("2nd argument must be a non-empty string"), ""
-	}
-	if len(args[2]) <= 0 {
-		return shim.Error("3rd argument must be a non-empty string"), ""
-	}
-	if len(args[3]) <= 0 {
-		return shim.Error("4th argument must be a non-empty string"), ""
-	}
-	if len(args[4]) <= 0 {
-		return shim.Error("5th argument must be a non-empty string"), ""
-	}
+    return shim.Error("1st argument must be a non-empty string"), ""
+  }
+  if len(args[1]) <= 0 {
+    return shim.Error("2nd argument must be a non-empty string"), ""
+  }
+  if len(args[2]) <= 0 {
+    return shim.Error("3rd argument must be a non-empty string"), ""
+  }
+  if len(args[3]) <= 0 {
+    return shim.Error("4th argument must be a non-empty string"), ""
+  }
+  if len(args[4]) <= 0 {
+    return shim.Error("5th argument must be a non-empty string"), ""
+  }
 
-	clientID := args[1]
-	act := args[2]
-	r := args[3]
-	s := args[4]
+  clientID := args[1]
+  act := args[2]
+  r := args[3]
+  s := args[4]
 
-	// ==== Create client compositekey ====
-	clientIndexName := "client"
-	clientIndexKey, err := stub.CreateCompositeKey(clientIndexName, []string{clientID})
-	if err != nil {
-		return shim.Error(err.Error()), ""
-	}
-	value := []byte{0x00}
+  // ==== Create client compositekey ====
+  clientIndexName := "client"
+  clientIndexKey, err := stub.CreateCompositeKey(clientIndexName, []string{clientID})
+  if err != nil {
+    return shim.Error(err.Error()), ""
+  }
+  value := []byte{0x00}
 
-	// ==== Check if client exists ====
-	clientAsBytes, err := stub.GetState(clientIndexKey)
-	if err != nil {
-		return shim.Error("Failed to get client: " + err.Error()), ""
-	} else if clientAsBytes == nil {
-		return shim.Error("Client doesn't exit, please check your client ID!"), ""
-	}
+  // ==== Check if client exists ====
+  clientAsBytes, err := stub.GetState(clientIndexKey)
+  if err != nil {
+    return shim.Error("Failed to get client: " + err.Error()), ""
+  } else if clientAsBytes == nil {
+    return shim.Error("Client doesn't exit, please check your client ID!"), ""
+  }
 
-	err = json.Unmarshal([]byte(clientAsBytes), &clientJSON)
-	if err != nil {
-		jsonResp := "{\"Error\":\"Failed to decode JSON of: " + clientID + "\"}"
-		return shim.Error(jsonResp), ""
-	}
+  err = json.Unmarshal([]byte(clientAsBytes), &clientJSON)
+  if err != nil {
+    jsonResp := "{\"Error\":\"Failed to decode JSON of: " + clientID + "\"}"
+    return shim.Error(jsonResp), ""
+  }
 
-	objectType := clientJSON.ObjectType
-	x := clientJSON.PublicKeyX
-	y := clientJSON.PublicKeyY
-	account := clientJSON.Account + 10000
+  objectType := clientJSON.ObjectType
+  x := clientJSON.PublicKeyX
+  y := clientJSON.PublicKeyY
+  account := clientJSON.Account + 10000
 
-	verify := verifySignature(x, y, r, s, act)
-	if verify == false {
-		return shim.Error("Verify failed!!!"), ""
-	}
+  verify := verifySignature(x, y, r, s, act)
+  if verify == false {
+    return shim.Error("Verify failed!!!"), ""
+  }
 
-	newClientJSON := &Client{clientID, objectType, x, y, account}
-	clientJSONAsBytes, err := json.Marshal(newClientJSON)
-	if err != nil {
-		return shim.Error(err.Error()), ""
-	}
-	stub.PutState(clientIndexKey, value)
+  newClientJSON := &Client{clientID, objectType, x, y, account}
+  clientJSONAsBytes, err := json.Marshal(newClientJSON)
+  if err != nil {
+    return shim.Error(err.Error()), ""
+  }
+  stub.PutState(clientIndexKey, value)
 
-	// ==== Save client to state ====
-	err = stub.PutState(clientIndexKey, clientJSONAsBytes)
-	if err != nil {
-		return shim.Error(err.Error()), ""
-	}
+  // ==== Save client to state ====
+  err = stub.PutState(clientIndexKey, clientJSONAsBytes)
+  if err != nil {
+    return shim.Error(err.Error()), ""
+  }
 
   return shim.Success(nil), "Done"
 }
